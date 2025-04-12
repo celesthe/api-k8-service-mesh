522ad34b65098ce96cd6e38e6a60dec7f7268e00
celesthe <celesthesazo@gmail.com>
2025-04-12 10:31:57 -0600

    feat: se agrego grafana u prometheus

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM node:18-alpine
 WORKDIR /app
 COPY package*.json ./
 RUN npm install
+RUN npm install prom-client
 COPY . .
 EXPOSE 3000
 CMD ["node", "index.js"]
--- index.js
+++ index.js
@@ -2,9 +2,21 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+const client = require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
+collectDefaultMetrics();
+
+
 app.get('/ping', (req, res) => {
   res.json({ message: 'pong' });
 });
+
+
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
 
 app.listen(port, () => {
   console.log(`API corriendo en http://localhost:${port}`);
